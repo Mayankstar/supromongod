e8115ea47a050a9ede4ae802d5680a93ae2560cd
Oleg Verych <olecom@gmail.com>
2014-10-15 07:22:33 +0300

    config default: store in module and assign by default if it simply enabled
    Signed-off-by: Oleg Verych <olecom@gmail.com>

--- app_back_supromongod.js
+++ app_back_supromongod.js
@@ -8,6 +8,10 @@ function supromongod(api, cfg){
 var app = api.app, name = 'supromongod'
    ,path = require('path')
 
+    if('boolean' == typeof cfg){// simple module enabler by 'true'
+        cfg = (config_default()).supromongod// use default config
+    }
+
     cfg.db_path = path.normalize(
         //  $PWD/app_modules/supromongod/ -> $PWD/data/supromongod/
         __dirname + '/../..' + (cfg.db_path || '/data/supromongod/')
@@ -45,5 +49,59 @@ var app = api.app, name = 'supromongod'
         app.use('/css/' + name + '/css', api.connect.sendFile(
             __dirname + '/' + name + '.css', true)
         )
+    }
+
+    function config_default(){
+        return {// config part as it can be in main config file
+        supromongod:{//'mongodb://' + process.env.MONGODS + process.env.MONGO_DBNAME
+            // comment out `bin` if `mongodb[.exe]` is launched elsewhere
+            bin: 'bin/mongod.exe',// if distro is used, put e.g. '/usr/bin/' here
+            stop_on_restart: !true,// if `node.js` restarts stop `mongod` or not
+            db_path: '/data/supromongod/',
+            cmd_launch: '',
+            log_filename: '',
+            url: 'mongodb://127.0.0.1:27727/'
+           ,extjs:{ mongodb_port: 27727 }
+           ,db_name: 'supro_GLOB'
+           ,options:{// you know what you are doing here!
+                db:{
+                    forceServerObjectId: true
+                   ,bufferMaxEntries: 0
+                   ,journal: true
+                }
+               ,server:{
+                    auto_reconnect: true
+                }
+            }
+           ,rbac:{
+                roles:{
+                    'mongo.role':[// new cans are merged
+                        'module.supromongod',
+                        '/supromongod/lib/',
+                        'App.supromongod.view.ControlTools',
+
+                        'App.um.wes',
+                        'App.um.controller.Chat',
+                        'App.um.view.Chat',
+                        '/um/lib/wes',
+                        '/um/lib/chat'
+                    ],
+                    'developer.local':[// add to existing role
+                        'module.supromongod',//it has '*' but anyway
+                        '/supromongod/lib/',// it has '*' but anyway
+                        'App.supromongod.view.ControlTools',
+                    ]
+                },
+                users:{
+                    'mongo':{
+                        id: 'mongo',
+                        // require('crypto').createHash('sha1').update(pass).digest('hex')
+                        pass: '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684',
+                        roles:['mongo.role'],
+                        name: 'mongo role'
+                    }
+                }
+            }
+        }}// stands before userman for now
     }
 }
