c76b84bb80390acf857dc0de5324b593c3ed28e2
Oleg Verych <ole4at@gmail.com>
2015-09-25 14:28:17 +0300

    mongo-export.sh: add gzip; fix $DB check
    Signed-off-by: Oleg Verych <ole4at@gmail.com>

--- etc/mongo-export.sh
+++ etc/mongo-export.sh
@@ -2,8 +2,9 @@
 # Export a collection using `mongoexport` and current config $DB
 # NOTE: "host:port" is hardcoded
 # $1 -- colection name
-# DB name is taken from $NODEJS_CONFIG or $2
-# output file name: "mongo_$1.json"
+# $2 -- DB name is taken from $NODEJS_CONFIG or $2
+# output file name: "mongo_$1.json[.gz]"
+# $3 -- if specified no gzip is used
 set -e
 
 trap 'echo "
@@ -27,7 +28,7 @@ if [ "$NODEJS_CONFIG" ]
 then
     # get database name from the javascript config file comment:
     #```js
-    #var DB  = 'hpoisk_nodes_' + OBJ // mongo-shell db: hpoisk_nodes_GLOB\r\n
+    #var DB  = 'nodes_' + OBJ // mongo-shell, mongo export db: nodes_GLOB\r\n
     #```                                                ^^^^^^^^^^^^^^^^^
     DB=${NODEJS_CONFIG##*var DB}
     n=`printf '\n\r'`
@@ -37,11 +38,23 @@ else
     DB=$2
 fi
 
-[ "$1" ] || echo '
+[ "$DB" ] || echo '
 WARNING: $DB is empty
 please specify $NODEJS_CONFIG or $2 as DB name
 '
 
+if type gzip >/dev/null && [ ! "$3" ]
+then
+echo "
+from '$DB' exporting '$1' && gzip => './mongo_$1.json.gz'
+...
+"
+"${0%/*}/../bin/mongoexport"             \
+              '--host' '127.0.0.1:27727' \
+              '--db' "$DB"               \
+              '--collection' "$1"        \
+    | gzip -9 >"mongo_$1.json.gz"
+else
 echo "
 from '$DB' exporting '$1' => './mongo_$1.json'
 ...
@@ -51,6 +64,7 @@ from '$DB' exporting '$1' => './mongo_$1.json'
               '--db' "$DB"               \
               '--collection' "$1"        \
               '--out' "mongo_$1.json"
+fi
 
 trap '' 0
 exit 0
